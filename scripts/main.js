76d08ea10aa7ed960d3c21b51030cf566f548bda
marstilen <tilen.mars@proton.me>
2025-12-03 11:32:37 +0100

    Calculate now works with basic operators

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,38 @@
-
-
 calcTextBox = document.querySelector("#calcTextBox");
 
-console.log(calcTextBox)
+console.log(calcTextBox);
 
-function calculate(params) {
-    
+function calculate(a, b, operator) {
+  console.log(operator);
+  switch (operator) {
+    case '+':
+      return a+b;
+    case '-':
+      return a-b;
+    case '*':
+      return a*b;
+    case '/': 
+      return a/b;
+    default:
+      return "";
+  }
+  console.log(a);
+  console.log(b);
 }
 
-document.addEventListener('click', function(e){
-    let firstNumber;
-let secondNumber;
-let operator;
-    console.log(e.target.getAttribute('class'));
-    if(e.target.getAttribute('class')=='button')
-    {
-        calcTextBox.value += e.target.getAttribute("value");
-    }
-    else if(e.target.getAttribute('class')=='operator'){
-        console.log(calcTextBox.value)
-        firstNumber = calcTextBox.value;
-        operator = e.target.getAttribute('class');
-        calcTextBox.value += e.target.getAttribute("value");
-    }
-    else if(e.target.getAttribute('class')=='clear'){
-        calcTextBox.value="";
-    }
-    else if(e.target.getAttribute('class')=='c'){
-        calcTextBox.value = calcTextBox.value.slice(0, -1)
-    }
-    else if(e.target.getAttribute('class')=='calculate')
-    {
-        console.log(operator)
-       calculate();
-    }
-})
+document.addEventListener("click", function (e) {
+  console.log(e.target.getAttribute("class"));
+  if (e.target.getAttribute("class") == "button") {
+    calcTextBox.value += e.target.getAttribute("value");
+  } else if (e.target.getAttribute("class") == "operator") {
+    calcTextBox.value += e.target.getAttribute("value");
+  } else if (e.target.getAttribute("class") == "clear") {
+    calcTextBox.value = "";
+  } else if (e.target.getAttribute("class") == "c") {
+    calcTextBox.value = calcTextBox.value.slice(0, -1);
+  } else if (e.target.getAttribute("class") == "calculate") {
+    let operatorIndex = calcTextBox.value.search(/[+\-*/]/);
+    let numbers = [...calcTextBox.value]
+    calcTextBox.value = calculate(numbers.slice(0,operatorIndex).join(''),numbers.slice(operatorIndex+1, numbers.length).join('') , numbers[operatorIndex]);
+  }
+});
